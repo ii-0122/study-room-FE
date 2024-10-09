1e6d9c7a87a64662a7a8875244055f89c1acace3
bang-wol <102708198+bang-wol@users.noreply.github.com>
2024-10-09 18:16:01 +0900

    [FEAT] 헤더에 사용자 프로필 이미지 표시  (#59)
    * remove: dDay 정보 삭제
    
    * feat: user 정보 쿠키에 저장
    
    * feat: 헤더에 프로필 이미지 추가
    
    * feat: 드롭다운 기능 추가
    
    * feat: 드롭다운 외부 클릭 감지하여 드롭다운 닫는 기능 추가
    
    * design: 드롭다운 서서히 열리고 닫히게 애니메이션 추가
    
    * fix: props DOM에 전달하지 않도록 변수 수정
    
    * design: 기본 이미지 색상 gray로 수정

--- src/components/header/Header.style.ts
+++ src/components/header/Header.style.ts
@@ -10,6 +10,7 @@ export const HeaderContainer = styled.header<{ isStudyRoomPage?: boolean }>`
   justify-content: space-between;
   align-items: center;
   padding: 32px;
+  position: relative;
 `;
 
 export const HeaderTitle = styled.h1`
@@ -39,4 +40,64 @@ export const Button = styled.button`
 export const ButtonWrapper = styled.div`
   display: flex;
   gap: 10px;
+`;
+
+export const ProfileImg = styled.img`
+  border-radius: 50%;
+  width: 32px;
+  height: 32px;
+  cursor: pointer;
+  border: 1px solid lightgray;
+`;
+
+export const DropdownProfileImg = styled.img`
+  border-radius: 50%;
+  width: 48px;
+  height: 48px;
+  border: 1px solid lightgray;
+`;
+
+export const DropDownWrapper = styled.div<{ $isOpen: boolean }>`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 70px;
+  right: 20px;
+  min-width: 240px;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  background-color: white;
+  opacity: ${({ $isOpen }) => ($isOpen ? 1 : 0)};
+  transform: translateY(${({ $isOpen }) => ($isOpen ? '0' : '-10px')});
+  transition:
+    opacity 0.3s ease,
+    transform 0.3s ease;
+`;
+
+export const UserInfo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  padding: 14px 20px;
+  border-bottom: 1px solid lightgray;
+  width: 100%;
+`;
+
+export const DropdownText = styled.p`
+  font-size: 16px;
+  font-weight: 500;
+`;
+
+export const DropdownItems = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  display: flex;
+  gap: 8px;
+  align-items: center;
+  padding: 14px 20px;
+  color: #616161;
+
+  &:hover {
+    background-color: #f4f4f4;
+  }
 `;
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,6 +3,12 @@ import { logout } from '@/apis/auth.api';
 import * as S from './Header.style';
 import { useAuthStore } from '@/stores/auth.store';
 import { toast } from 'react-toastify';
+import { useEffect, useRef, useState } from 'react';
+import {
+  IoPersonCircle,
+  IoSettingsOutline,
+  IoLogOutOutline,
+} from 'react-icons/io5';
 
 interface HeaderProps {
   title: string;
@@ -10,7 +16,28 @@ interface HeaderProps {
 
 export default function Header({ title }: HeaderProps) {
   const navigate = useNavigate();
-  const { accessToken, clearAuthData } = useAuthStore();
+  const { accessToken, user, clearAuthData } = useAuthStore();
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement | null>(null);
+  const profileRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(e.target as Node) &&
+        profileRef.current &&
+        !profileRef.current.contains(e.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
 
   const handleLogout = async () => {
     await logout();
@@ -19,14 +46,43 @@ export default function Header({ title }: HeaderProps) {
     navigate('/');
   };
 
+  const toggleDropdown = () => setIsOpen((prev) => !prev);
+
+  const renderProfileImage = () => {
+    return user?.imageUrl ? (
+      <S.ProfileImg src={user?.imageUrl} alt="Profile" />
+    ) : (
+      <IoPersonCircle size={32} color="gray" />
+    );
+  };
+
+  const renderDropdown = () => {
+    return (
+      <S.DropDownWrapper ref={dropdownRef} $isOpen={isOpen}>
+        <S.UserInfo>
+          {renderProfileImage()}
+          <S.DropdownText>{user?.nickname} 님</S.DropdownText>
+        </S.UserInfo>
+        <S.DropdownItems onClick={() => navigate('/profile')}>
+          <IoSettingsOutline /> 프로필 수정
+        </S.DropdownItems>
+        <S.DropdownItems onClick={handleLogout}>
+          <IoLogOutOutline /> 로그아웃
+        </S.DropdownItems>
+      </S.DropDownWrapper>
+    );
+  };
+
   return (
     <S.HeaderContainer>
       <S.HeaderTitle>{title}</S.HeaderTitle>
       <S.ButtonWrapper>
         {accessToken ? (
           <>
-            <S.Button onClick={handleLogout}>로그아웃</S.Button>
-            <S.Button onClick={() => navigate('/profile')}>프로필</S.Button>
+            <div ref={profileRef} onClick={toggleDropdown}>
+              {renderProfileImage()}
+            </div>
+            {renderDropdown()}
           </>
         ) : (
           <>
